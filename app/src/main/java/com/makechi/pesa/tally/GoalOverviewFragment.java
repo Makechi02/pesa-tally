90b834de60ac042e7500280c6c7105cdd58f48a4
Makechi <makechieric9@gmail.com>
2025-01-05 18:01:14 +0300

    Refactor GoalOverviewFragment layout and remove unused views
    Replaced `FrameLayout` with a centered `LinearLayout` for improved structure and added saved/target TextViews to display progress. Removed unused `ProgressBar` and related code to simplify UI logic. Adjusted logic to handle goal details with new formatting.

--- app/src/main/java/com/makechi/pesa/tally/GoalOverviewFragment.java
+++ app/src/main/java/com/makechi/pesa/tally/GoalOverviewFragment.java
@@ -4,24 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import com.makechi.pesa.tally.entity.Goal;
+import com.makechi.pesa.tally.util.Formatter;
 import com.makechi.pesa.tally.viewModel.GoalViewModel;
 
 public class GoalOverviewFragment extends Fragment {
 
     private TextView descriptionTextView;
-    private TextView progressTextView;
     private TextView savedTextView;
     private TextView targetTextView;
     private TextView remainingTextView;
     private TextView deadlineTextView;
     private TextView transactionCountTextView;
-    private ProgressBar progressBar;
     private JarView jarView;
 
     public GoalOverviewFragment() {
@@ -55,20 +53,17 @@ public class GoalOverviewFragment extends Fragment {
         int goalId = bundle.getInt("GOAL_ID", -1);
 
         descriptionTextView = view.findViewById(R.id.text_view_goal_description);
-//        savedTextView = view.findViewById(R.id.text_view_goal_saved);
-//        targetTextView = view.findViewById(R.id.text_view_target);
+        savedTextView = view.findViewById(R.id.text_view_goal_saved);
+        targetTextView = view.findViewById(R.id.text_view_target);
 //        remainingTextView = view.findViewById(R.id.text_view_remaining);
         deadlineTextView = view.findViewById(R.id.text_view_goal_deadline);
         transactionCountTextView = view.findViewById(R.id.text_view_goal_transactions);
-//        progressBar = view.findViewById(R.id.progress_goal);
-//        progressTextView = view.findViewById(R.id.text_progress);
         jarView = view.findViewById(R.id.jar_view);
 
         GoalViewModel goalViewModel = new ViewModelProvider(this).get(GoalViewModel.class);
         goalViewModel.getGoalById(goalId).observe(this, this::populateGoalDetails);
         goalViewModel.getTransactionByGoal(goalId).observe(this, transactions -> {
             int count = transactions.size();
-
             transactionCountTextView.setText(String.valueOf(count));
         });
 
@@ -80,15 +75,13 @@ public class GoalOverviewFragment extends Fragment {
             double remainingAmount = goal.getTargetAmount() - goal.getAmountSaved();
 
             descriptionTextView.setText(goal.getDescription());
-//            savedTextView.setText(Formatter.formatMoneyWithCurrency(goal.getAmountSaved()));
-//            targetTextView.setText(Formatter.formatMoneyWithCurrency(goal.getTargetAmount()));
+            savedTextView.setText(Formatter.formatMoneyWithCurrency(goal.getAmountSaved()));
+            targetTextView.setText(Formatter.formatMoneyWithCurrency(goal.getTargetAmount()));
 //            remainingTextView.setText(Formatter.formatMoneyWithCurrency(remainingAmount));
             deadlineTextView.setText(goal.getDeadline());
 
             float progress = (float) (goal.getAmountSaved() / goal.getTargetAmount());
             jarView.setProgress(progress);
-//            progressBar.setProgress(progress);
-//            progressTextView.setText(progress + "%");
         } else {
             showErrorToast("Failed to fetch goal details");
         }
